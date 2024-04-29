0fe384a8dec0fd84647bd5c0d799547b82a37906
EChambeee <e.elizabeth.chamberlain@gmail.com>
2024-04-29 09:51:28 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+    Random randomGenerator = new Random();
+    int number = randomGenerator.Next(1, 101);
+
+        int guessNumber;
+        string HigherLower = "";
+
+        while (true)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessNumber = int.Parse(guess);
+
+            if (guessNumber == number)
+            {
+                Console.WriteLine("You guessed it!");
+                break; 
+            }
+            else if (guessNumber < number)
+            {
+                HigherLower = "Higher";
+            }
+            else if (guessNumber > number)
+            {
+                HigherLower = "Lower";
+            }
+
+            Console.WriteLine(HigherLower);
+        }
     }
 }
